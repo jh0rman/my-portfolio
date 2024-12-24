c8cdf152dbb8ef2d5b145700023599b10400ed94
Jhorman <jhormanrus@gmail.com>
2024-12-24 04:14:19 +0000

    📦 setup tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config'
 import astroI18next from 'astro-i18next'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()]
+  integrations: [astroI18next()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
